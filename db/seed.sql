71b92e7b248b7635300cccb4fceff54f928998a8
dianaz <stingray2091@gmail.com>
2020-06-24 19:40:56 -0400

    created different folders

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO bugers (burger_name, devoured) VALUES ('bacon burger',true);
+INSERT INTO bugers (burger_name, devoured) VALUES ('double cheese burger', false);
+INSERT INTO bugers (burger_name, devoured) VALUES ('veggie burger',true);
